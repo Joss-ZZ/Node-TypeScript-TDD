b271b577ded13ea6bc7811685a9d0e037b2458ff
Jhosimar <jzevallos@sintad.pe>
2021-06-29 19:40:49 -0500

    chore: implementation interfaces

--- src/interfaces/ITeam.ts
+++ src/interfaces/ITeam.ts
@@ -1,6 +1,6 @@
 export interface ISoccerPlay {
-  attacker: (name: string) => any
+  attacker: (name: string) => void
 }
 export interface ITeam {
-  position: (name: string, tShirt: number, skills: any[]) => any
+  position: (name: string, tShirt: number, skills: any[]) => void
 }
